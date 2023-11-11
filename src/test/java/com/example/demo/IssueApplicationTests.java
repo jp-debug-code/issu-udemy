e211cfac1788307d93c439fd2c9ee62699467bc0
jp-debug-code <kiminoakari@gmail.com>
2023-11-12 00:20:11 +0900

    3

--- src/test/java/com/example/demo/IssueApplicationTests.java
+++ src/test/java/com/example/demo/IssueApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.demo;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.LoadState;
+import com.microsoft.playwright.options.WaitUntilState;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.microsoft.playwright.Browser.*;
+
 
 @SpringBootTest
 class IssueApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void myPlaywrightTest(){
+
+
+		try(Playwright playwright=Playwright.create()){
+				Browser browser= playwright.chromium().launch();
+			BrowserContext context = browser.newContext(new Browser.NewContextOptions());
+			Page page=context.newPage();
+				page.navigate("http://localhost:8080",new Page.NavigateOptions().setWaitUntil(WaitUntilState.DOMCONTENTLOADED));
+			page.locator("a").getByText("課題一覧").first().click();
+
+
+
+
+				}
+
+
+
 	}
 
 }
