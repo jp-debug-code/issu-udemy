41f15590d9cdbc2820842532fd0d9a219f3070f3
jp-debug-code <kiminoakari@gmail.com>
2023-11-12 12:25:36 +0900

    4

--- src/test/java/com/example/demo/IssueApplicationTests.java
+++ src/test/java/com/example/demo/IssueApplicationTests.java
@@ -20,7 +20,7 @@ import static com.microsoft.playwright.Browser.*;
 class IssueApplicationTests {
 
 	@Test
-	void myPlaywrightTest(){
+	void myPlaywrightTest(){//作成する
 
 
 		try(Playwright playwright=Playwright.create()){
@@ -29,14 +29,45 @@ class IssueApplicationTests {
 			Page page=context.newPage();
 				page.navigate("http://localhost:8080",new Page.NavigateOptions().setWaitUntil(WaitUntilState.DOMCONTENTLOADED));
 			page.locator("a").getByText("課題一覧").first().click();
-
+			page.locator("a").getByText("作成").first().click();
+			page.locator("#ss").fill("tsssss");
+			page.locator("#description").fill("tsssss");
+			page.locator(".btn.btn-primary").click();
 
 
 
 				}
+	}
 
+		@Test
+	void test(){ //作成キャンセル
+			try(Playwright playwright=Playwright.create()) {
+				Browser browser = playwright.chromium().launch();
+				BrowserContext context = browser.newContext(new Browser.NewContextOptions());
+				Page page = context.newPage();
+				page.navigate("http://localhost:8080", new Page.NavigateOptions().setWaitUntil(WaitUntilState.DOMCONTENTLOADED));
+				page.locator("a").getByText("課題一覧").first().click();
+				page.locator("a").getByText("作成").first().click();
+				page.locator("#ss").fill("delete");
+				page.locator("#description").fill("tsssss");
+				page.locator(".btn.btn-secondary").click();
+			}
 
 
 	}
+	@Test
+	void test3(){
+		try(Playwright playwright=Playwright.create()) {
+			Browser browser = playwright.chromium().launch();
+			BrowserContext context = browser.newContext(new Browser.NewContextOptions());
+			Page page = context.newPage();
+			page.navigate("http://localhost:8080", new Page.NavigateOptions().setWaitUntil(WaitUntilState.DOMCONTENTLOADED));
+			page.locator("a").getByText("課題一覧").first().click();
+			//page.locator("form").getByText("削除").first().click();
+			page.locator("form[action='/issues/delete/4']").getByText("削除").first().click();
+
+
+
+	}}
 
 }
